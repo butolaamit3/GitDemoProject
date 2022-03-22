42baef264498fc5284af2f1016bb2c17abf005a7
Amit Butola <butolaamit3@gmail.com>
2022-03-22 21:29:33 +0530

    Commit2: Adding 2 new java code in devTeam1 branch

---
+++ ex3.java
@@ -0,0 +1,5 @@
+public class ex3{
+    public static void main(String[] args){
+        System.out.println("Hey this is ex3 of branch devTeam1 branch");
+    }
+}
---
+++ ex4.java
@@ -0,0 +1,6 @@
+public class ex4 {
+    public static void main(String[] args){
+        System.out.println("This is the ex4 of devTeam1 branch");
+    }
+    
+}
