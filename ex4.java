d0d22d8b9f5b3a05d98838c64164d270edb0df04
Amit Butola <butolaamit3@gmail.com>
2022-03-23 21:15:12 +0530

    Commit5: updated z variable value

--- ex4.java
+++ ex4.java
@@ -1,7 +1,8 @@
 public class ex4 {
     public static void main(String[] args){
-        int z =700;
+        int a = 500;
         System.out.println("This is the ex4 of techDev branch for merge conflict");
     }
+
     
 }
