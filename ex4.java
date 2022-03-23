e0856332f6837757c50ec43bbe68232db20ace89
Amit Butola <butolaamit3@gmail.com>
2022-03-23 21:40:59 +0530

    Added new variable b

--- ex4.java
+++ ex4.java
@@ -1,7 +1,8 @@
 public class ex4 {
     public static void main(String[] args){
         int a = 500;
-        System.out.println("This is the ex4 of techDev branch for merge conflict");
+        int b = 600;
+        System.out.println("This is the ex4 of techDev branch for merge conflict"+a+b);
     }
 
     
