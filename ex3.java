2de83a8b40c4c9c598d9f99e3011a9009f24d4ca
Amit Butola <butolaamit3@gmail.com>
2022-03-23 20:56:10 +0530

    Commit4: added new code

--- ex3.java
+++ ex3.java
@@ -1,5 +1,5 @@
 public class ex3{
     public static void main(String[] args){
-        System.out.println("Hey this is ex3 of branch devTeam1 for merge conflict");
+        System.out.println("Hey this is ex3 of branch techDev branch for merge conflict");
     }
 }
