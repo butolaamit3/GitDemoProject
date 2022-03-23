852b9d0249977acb8ae4f40a27567cbe31ef4b16
Amit Butola <butolaamit3@gmail.com>
2022-03-23 20:53:32 +0530

    Commit3: Changes the code

--- ex4.java
+++ ex4.java
@@ -1,6 +1,6 @@
 public class ex4 {
     public static void main(String[] args){
-        System.out.println("This is the ex4 of devTeam1 branch");
+        System.out.println("This is the ex4 of techDev branch for merge conflict");
     }
     
 }
