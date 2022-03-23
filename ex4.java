27587f5b373699afdac775bac15211d86bc835dd
Amit Butola <butolaamit3@gmail.com>
2022-03-23 21:08:59 +0530

    Added z variable

--- ex4.java
+++ ex4.java
@@ -1,5 +1,6 @@
 public class ex4 {
     public static void main(String[] args){
+        int z =700;
         System.out.println("This is the ex4 of techDev branch for merge conflict");
     }
     
