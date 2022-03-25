6205b53d65f6000d7e613521002ffdf45292d0b8
Amit Butola <butolaamit3@gmail.com>
2022-03-25 13:29:01 +0530

    Updater ex3
    Changed the code

--- ex3.java
+++ ex3.java
@@ -1,5 +1,6 @@
 public class ex3{
     public static void main(String[] args){
         System.out.println("Hey this is ex3 of branch techDev branch for merge conflict");
+        System.out.println("This is for to merge a branch in remote repo");
     }
 }
