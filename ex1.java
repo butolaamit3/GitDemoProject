d190b8a035edf74b6de5a96ae7a884f526ddaa03
Amit Butola <butolaamit3@gmail.com>
2022-03-25 13:05:32 +0530

    Updated Ex1
    I mane a change in my remote repo.

--- ex1.java
+++ ex1.java
@@ -1,5 +1,6 @@
 public class ex1{
     public static void main(String[] args){
         System.out.println("Hey this is ex1");
+        System.out.println("Hey this is ex1 and I update it in my remote repo");
     }
 }
